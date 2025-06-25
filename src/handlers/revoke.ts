b4e6f8499f09ccb8b0e2e0e43a6a14ba76d22c18
feruzboboqulov02 <feruzboboqulov002@gmail.com>
2025-06-25 19:21:01 +0500

    deletions

--- src/handlers/revoke.ts
+++ src/handlers/revoke.ts
@@ -5,7 +5,7 @@ import { setCachedPermissions } from '../services/cache';
 import { log } from '../utils/logger';
 
 export const handleRevoke = async (msg: Msg) => {
-    console.log('🔁 Revoke handler triggered');
+    console.log('Revoke handler triggered');
 
   try {
     const { apiKey, module, action } = JSON.parse(msg.data.toString()) as GrantRevokeRequest;
